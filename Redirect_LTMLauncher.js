5e4d4cd4eed2dac108aa8c60c2b4170f49d74276
AndyHsieh <andy.nba85@gmail.com>
2021-07-15 17:55:50 +0800

    Update Redirect_LTMLauncher.js

--- Redirect_LTMLauncher.js
+++ Redirect_LTMLauncher.js
@@ -46,11 +46,12 @@ function navigateLTM(androidStoreId,appStoreId,recid) {
         //		window.location.href = `itms-apps://apps.apple.com/us/app/link-to-myasus/id${appStoreId}`;
         //	};
 	//},2000)
-	window.location.href = "linktomyasus://";
-	setTimeout(function(){
-		window.location.href = `https://apps.apple.com/app/link-to-myasus/id${appStoreId}?ls=1&mt=8`;
-		window.location.href = `https://apps.apple.com/app/link-to-myasus/id${appStoreId}?ls=1&mt=8`; 
-	},2000)
+	//window.location.href = "linktomyasus://";
+	window.location.href = "https://apps.apple.com/app/link-to-myasus/id${appStoreId}?ls=1&mt=8";
+	//setTimeout(function(){
+	//	window.location.href = `https://apps.apple.com/app/link-to-myasus/id${appStoreId}?ls=1&mt=8`;
+	//	window.location.href = `https://apps.apple.com/app/link-to-myasus/id${appStoreId}?ls=1&mt=8`; 
+	//},2000)
 	//window.location.replace('https://apps.apple.com/us/app/link-to-myasus/id1481035824?ls=1&mt=8');         
 
 		
