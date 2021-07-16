42889c2fd82e0857cb26a6bf5b1aa0c9802c2025
AndyHsieh <andy.nba85@gmail.com>
2021-07-16 14:02:06 +0800

    Update Redirect_LTMLauncher.js

--- Redirect_LTMLauncher.js
+++ Redirect_LTMLauncher.js
@@ -56,15 +56,14 @@ function navigateLTM(androidStoreId,appStoreId,recid) {
 		//window.location.replace(`https://apps.apple.com/us/app/link-to-myasus/id${appStoreId}?ls=1&mt=8`); 
 	},2000);
 	    
-	document.addEventListener("visibilitychange", function() {
-  		if (document.visibilityState === 'visible') {
-    			alert(1);
-			clearTimeout(timer);
-  		} else {
-			alert(2);
-    			//backgroundMusic.pause();
-  		}
-	});
+	 const visibilitychange = function() {
+		alert('1');
+  	 	const tag = document.hidden || document.webkitHidden;
+  		tag && clearTimeout(timer);
+	 };
+
+	document.addEventListener("visibilitychange", visibilitychange, false);
+	document.addEventListener("webkitvisibilitychange", visibilitychange, false);
 	    
 	
 	//}
